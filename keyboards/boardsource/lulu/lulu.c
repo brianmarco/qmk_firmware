3d504ef2d73f52b73795780b7e0645cf0a3a8799
Brian Marco <brianmarco@gmail.com>
2024-09-26 08:05:36 -0400

    wip

--- keyboards/boardsource/lulu/lulu.c
+++ keyboards/boardsource/lulu/lulu.c
@@ -5,12 +5,14 @@
 
 #ifdef ENCODER_ENABLE
 bool encoder_update_kb(uint8_t index, bool clockwise) {
-    if (!encoder_update_user(index, clockwise)) { return false; }
+    if (!encoder_update_user(index, clockwise)) {
+        return false;
+    }
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
             tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
         }
     } else if (index == 1) {
         if (clockwise) {
