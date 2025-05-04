251340e3bbbb1161b6064968dc0303b72e36c18e
Brian Marco <brianmarco@gmail.com>
2025-05-04 18:45:10 -0400

    wip

--- keyboards/boardsource/unicorne/keymaps/bmm/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/bmm/keymap.c
@@ -219,23 +219,5 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
         mouse_report.v = 0;
     }
 
-    // if (!layer_state_is(_MISC)) {
-    //     // Calculate and accumulate scroll values based on mouse movement and divisors
-    //     scroll_accumulated_h += (float)mouse_report.x / SCROLL_DIVISOR_H;
-    //     scroll_accumulated_v += (float)mouse_report.y / SCROLL_DIVISOR_V;
-    //
-    //     // Assign integer parts of accumulated scroll values to the mouse report
-    //     mouse_report.h = (int8_t)scroll_accumulated_h;
-    //     mouse_report.v = (int8_t)scroll_accumulated_v * -1;
-    //
-    //     // Update accumulated scroll values by subtracting the integer parts
-    //     scroll_accumulated_h -= (int8_t)scroll_accumulated_h;
-    //     scroll_accumulated_v -= (int8_t)scroll_accumulated_v;
-    //
-    //     // Clear the X and Y values of the mouse report
-    //     mouse_report.x = 0;
-    //     mouse_report.y = 0;
-    // }
-
     return mouse_report;
 }
